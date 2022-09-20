5f89c3c1870b4c97fa3ee804117861618b752978
Jane Gao <tianxiag@andrew.cmu.edu>
2022-09-20 00:10:41 -0700

    profile page

--- app/src/main/java/com/cmu/evplan/ProfileFragment.kt
+++ app/src/main/java/com/cmu/evplan/ProfileFragment.kt
@@ -5,16 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.preference.PreferenceFragmentCompat
+import com.cmu.evplan.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
